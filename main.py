290630cffa5b2d9701ca79bf1513476bba943eeb
Samuel <samuel.johannsen@tu-dortmund.de>
2023-11-14 20:58:05 +0100

    Refactor main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
 import block0
-import block0v2
 import block1
 import md5
 import hashlib
+import os
 from multiprocessing import Pool, cpu_count
 
 MD5IV = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476]
@@ -21,11 +21,23 @@ def test():
 
 
 def main():
+    newDir = os.getcwd() + "\\collisions"
+    if not os.path.exists(newDir):
+        os.makedirs(newDir)
+    result = [1234]
+    filePath = os.getcwd() + "\\collisions\\collisions.txt"
+    with open(filePath, "a+") as file:
+        file.write("\n%s\n" % result)
+        file.close()
+
     IV = MD5IV.copy()
     cpuCount = int(cpu_count() / 1.5)
     with Pool(cpuCount) as p:
         p.map(find_collision, [IV] * cpuCount)
         p.terminate()
+
+    print("Done. Quitting...")
+
 
 
 def find_collision(IV):
@@ -47,8 +59,16 @@ def find_collision(IV):
     msg2_block1[11] -= 1 << 15
     msg2_block1[14] += 1 << 31
 
-    print("DONE!!!")
-    return [(msg1_block0, msg1_block1), (msg2_block0, msg2_block1)]
+    print("Found collision, saving...")
+    # print(msg1_block0, msg1_block1)
+    # print(msg2_block0, msg2_block1)
+
+    result = [(hex(msg1_block0), hex(msg1_block1)), (hex(msg2_block0), hex(msg2_block1))]
+    filePath = os.getcwd() + "\\collisions\\collisions.txt"
+    with open(filePath, "a+") as file:
+        file.write("%s\n" % result)
+        file.close()
+
 
 
 if __name__ == '__main__':
